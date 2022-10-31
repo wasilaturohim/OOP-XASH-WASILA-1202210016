ba3c3686fd3dfa3ae7f4c803e280ade2dc44e8fb
wasilaturohim <91681144+wasilaturohim@users.noreply.github.com>
2022-10-31 08:55:35 +0700

    Add files via upload

---
+++ OOP_XASH_WASILA_1202210016_MODUL1/src/Database1.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Database1 {
+    public static void main(String[] args) throws Exception {
+
+        String 
+        System.out.println("Selamat Datang di Restoran EAD");
+        System.out.println("Silahkan Register Terlebih Dahulu");
+        System.out.println("==================================");
+
+
+    }
+}
