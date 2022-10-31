ed000fb20dcd0dcff49008527cfd9a968a986368
wasilaturohim <91681144+wasilaturohim@users.noreply.github.com>
2022-10-31 08:50:47 +0700

    Add files via upload

---
+++ MODUL 1/src/server.java
@@ -0,0 +1,9 @@
+public class server {
+    Scanner input = new Scanner(System.in);
+    System.out.print("Nama : ");
+    String nama = input.nextLine();
+
+    System.out.print("Nomor HP : ");
+    Int phone = input.nextLine();
+    
+}
