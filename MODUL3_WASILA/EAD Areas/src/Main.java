93bc5dde32e0a7d4b29885a0fc041c4642b910f6
wasilaturohim <91681144+wasilaturohim@users.noreply.github.com>
2022-12-26 08:51:36 +0700

    Add files via upload

---
+++ MODUL3_WASILA/EAD Areas/src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) throws Exception {
+        Calculation h = new Calculation();
+        Thread thread = new Thread();
+        Scanner sc = new Main();
+
+
+    }
+}
